dcf1450c4c16540378ce268d7c077bfe4f38c365
andrew-atkinson <blind.sam@gmail.com>
2017-05-29 18:14:37 -0400

    second attempt

---
+++ GuessingGame.js
@@ -0,0 +1,120 @@
+function generateWinningNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function shuffle(arr) {
+    var m = arr.length,
+        i;
+    // While there remain elements to shuffle…
+    while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+        // And swap it with the current element.
+        [arr[m], arr[i]] = [arr[i], arr[m]];
+    }
+    return arr;
+}
+
+function Game() {
+    this.playersGuess = null;
+    this.pastGuesses = [];
+    this.winningNumber = generateWinningNumber();
+}
+
+Game.prototype.difference = function() {
+    return Math.abs(this.playersGuess - this.winningNumber);
+}
+
+Game.prototype.isLower = function() {
+    return this.playersGuess < this.winningNumber;
+}
+
+Game.prototype.playersGuessSubmission = function(num) {
+    console.log(num);
+    if (typeof num !== 'number' || (1 > num) || (num > 100)) {
+        throw "That is an invalid guess."
+    }
+    this.playersGuess = num;
+    return this.checkGuess();
+}
+
+Game.prototype.checkGuess = function() {
+    var swatchColor = "";
+    var numberColor = "";
+    var subtitleColor = "";
+    if (this.playersGuess === this.winningNumber) {
+        $("#title").text("You Win!").css({ "color": "#FF6C03", "font-size": "7em"});
+        $("#subtitle").text("click reset to play again.");
+        var swatchColor = "FF8928";
+        numberColor = "#7F3601";
+    } else if (this.pastGuesses.indexOf(this.playersGuess) > -1) {
+        $("#title").text("You have already guessed that number.").css("font-size", "4em");
+    } else if (this.pastGuesses.length > 4) {
+        $("#title").text("You Lose!");
+        $("#subtitle").text("click reset to play again.");
+        $('#hint, #submit').prop("disabled", true);
+        // return "You Lose."
+    } else {
+        this.pastGuesses.push(this.playersGuess);
+        $('#guesses ul li:nth-child(' + this.pastGuesses.length + ')').text(this.playersGuess);
+        var diffThisTurn = this.difference();
+        if (diffThisTurn <= 10) {
+            swatchColor = "#E8CA68";
+            numberColor = "#664F05";
+            subtitleColor = "#E5B10B";
+            $("#subtitle").text("You\'re burning up!");
+        } else if (diffThisTurn < 25) {
+            swatchColor = "#E5FF64";
+            numberColor = "#6A7F00";
+            subtitleColor = "#D5FF00";
+            $("#subtitle").text("You\'re lukewarm");
+        } else if (diffThisTurn < 50) {
+            swatchColor = "#6EE87B";
+            numberColor = "#027512";
+            subtitleColor = "#05F526";
+            $("#subtitle").text("You\'re a bit chilly.");
+        } else {
+            swatchColor = "#29FFE7";
+            numberColor = "#037F79";
+            subtitleColor = "#05FFF3";
+            $("#subtitle").text("You\'re ice cold!");
+        }
+        $('#guesses ul li:nth-child(' + this.pastGuesses.length + ')').css({ "background-color": swatchColor, "color": numberColor });
+        $("#subtitle").css({ "color": subtitleColor });
+    }
+}
+
+Game.prototype.provideHint = function() {
+    var hintArr = [this.winningNumber, generateWinningNumber(), generateWinningNumber()];
+    // return shuffle(hintArr);
+    return shuffle(hintArr);
+}
+
+function newGame() {
+    return new Game();
+}
+
+
+$(document).ready(function() {
+    var game = newGame();
+    $('#submit').on('click', function() {
+        var submission = +$('#player-input').val();
+        $('#player-input').val('');
+        var output = game.playersGuessSubmission(submission);
+        console.log(output);
+    })
+    $("#hint").on('click', function() {
+        var hintText = game.provideHint();
+        $("#title").text('The winning number is either '+hintText[0]+", "+hintText[1]+", or "+hintText[2]).css({"font-size": "3em"});
+        console.log(hintText);
+        
+    })
+    $('#reset').on('click', function() {
+        game = newGame();
+        $("#guesses li").text("-").css({"color": "#554","background": "#dfc"});
+        $("#title"). text("Guessing Game").css({"font-size": "7em","color": "#dfc"});
+        $("#subtitle").text("Guess a number between one and a hundred").css({"color": "#fff", "font-size": "3em"});
+        $('#hint, #submit').prop("disabled",false);
+        console.log('new game...');
+    })
+})
