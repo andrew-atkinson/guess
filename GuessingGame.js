a8aa077930012a75380bca78855147cde26526bb
andrew-atkinson <blind.sam@gmail.com>
2017-05-30 09:45:53 -0400

    updated css

--- GuessingGame.js
+++ GuessingGame.js
@@ -3,7 +3,8 @@ function generateWinningNumber() {
 }
 
 function shuffle(arr) {
-    var m = arr.length, i;
+    var m = arr.length,
+        i;
     // While there remain elements to shuffle…
     while (m) {
         // Pick a remaining element…
@@ -42,7 +43,7 @@ Game.prototype.playersGuessSubmission = function(num) {
 
 Game.prototype.checkGuess = function() {
     if (this.playersGuess === this.winningNumber) {
-        $("#title").text("You Win!").css({ "color": "#FF6C03", "font-size": "7em"}).fadeIn();
+        $("#title").text("You Win!").css({ "color": "#FF6C03", "font-size": "7em" }).fadeIn();
         $("#subtitle").text("click reset to play again.");
         $('#hint, #submit, #player-input').prop("disabled", true);
         this.swatchColor = "FF8928";
@@ -107,16 +108,16 @@ $(document).ready(function() {
     })
     $("#hint").on('click', function() {
         var hintText = game.provideHint();
-        $("#title").text('The winning number is either '+hintText[0]+", "+hintText[1]+", or "+hintText[2]).css({"font-size": "3em"});
+        $("#title").text('The winning number is either ' + hintText[0] + ", " + hintText[1] + ", or " + hintText[2]).css({ "font-size": "3em" });
         console.log(hintText);
-        
+
     })
     $('#reset').on('click', function() {
         game = newGame();
-        $("#guesses li").text("-").css({"color": "#554","background": "#dfc"});
-        $("#title"). text("Guessing Game").css({"font-size": "7em","color": "#dfc"});
-        $("#subtitle").text("Guess a number between one and a hundred").css({"color": "#fff", "font-size": "3em"});
-        $('#hint, #submit, #player-input').prop("disabled",false);
+        $("#guesses li").text("-").css({ "color": "#554", "background": "#dfc" });
+        $("#title").text("Guessing Game").css({ "font-size": "7em", "color": "#dfc" });
+        $("#subtitle").text("Guess a number between one and a hundred").css({ "color": "#fff", "font-size": "3em" });
+        $('#hint, #submit, #player-input').prop("disabled", false);
         console.log('new game...');
     })
 })
